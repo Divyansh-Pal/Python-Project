cde056712b70062eac29ae81fb79f9a60f0fec8e
Divyansh pal <88077772+Divyansh-Pal@users.noreply.github.com>
2021-08-05 15:00:49 +0530

    Add files via upload

---
+++ simple-interest.py
@@ -0,0 +1,7 @@
+#WAP to calculate Simple Interest (SI)
+
+P= int(input("Enter Principal amount here:"))
+R= float(input("Enter Rate of Interest here:"))
+T= float(input("Enter Time here:"))
+
+print(P*R*T/100)
---
+++ work.py
@@ -0,0 +1,12 @@
+#WAP to do mathematic calculation
+
+O= float(input("Enter first number here:"))
+T= float(input("Enter second number here:"))
+
+print(O+T)
+print(O-T)
+print(O*T)
+print(O/T)
+print("1.Addition 2.Subtraction 3.Multiplication 4.Division")
+
+    
